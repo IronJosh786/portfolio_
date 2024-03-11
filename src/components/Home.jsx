1c1547be99d06762485dfe80e5f46d742daa0f51
Faizan <sfaizan78692@gmail.com>
2024-03-11 13:36:32 +0530

    add new project and technology use in project section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,44 @@
 import { Link } from "react-router-dom";
-import HeroImage from '../assets/heroImage.webp'
+import HeroImage from "../assets/heroImage.webp";
 
 export default function Home() {
-    return(
-        <div className="flex flex-col gap-4 border-l-2 border-pink-700 px-6">
-            <img src={HeroImage} alt="Hero Image" className=" h-40 w-40 md:h-60 md:w-60 rounded-full"/>
-            <h2 className="text-3xl md:text-5xl font-bold tracking-tight">Hi 👋 I'm Faizan</h2>
-            <h3 className="text-xl md:text-3xl font-medium tracking-tight"><span className="font-bold text-pink-700 selection:bg-pink-700 selection:text-white">FRONTEND WEB DEVELOPER</span> from Mumbai.</h3>
-            <p className="max-w-[60ch] text-base md:text-lg">I'm a <span className="text-pink-700 selection:bg-pink-700 selection:text-white">UI/UX</span> enthusiast dedicated to crafting visually stunning, pixel-perfect, and interactive websites. My goal is to create seamless user experiences, bringing digital visions to life. With a strong foundation in frontend technologies, I'm eager to explore new tools and techniques, delivering innovative solutions.</p>
-            <button className="flex justify-start"><Link to='mailto: faizanejazshaikh%40gmail%2Ecom' className="font-medium tracking-tighter rounded-full bg-pink-700 text-white p-2 text-base md:text-lg duration-300 shadow-lg shadow-pink-500 hover:-translate-y-1">Contact Me</Link></button>
-        </div>
-    );
+  return (
+    <div className="mt-8 flex flex-col gap-4 border-l-2 border-pink-700 px-6">
+      <img
+        src={HeroImage}
+        alt="Hero Image"
+        className=" h-40 w-40 md:h-60 md:w-60 rounded-full"
+      />
+      <h2 className="text-3xl md:text-5xl font-bold tracking-tight">
+        Hi 👋 I'm Faizan
+      </h2>
+      <h3 className="text-xl md:text-3xl font-medium tracking-tight">
+        <span className="font-bold text-pink-700 selection:bg-pink-700 selection:text-white">
+          FULL STACK DEVELOPER
+        </span>{" "}
+        from Mumbai.
+      </h3>
+      <p className="max-w-[60ch] text-base md:text-lg">
+        I'm a{" "}
+        <span className="text-pink-700 selection:bg-pink-700 selection:text-white">
+          passionate developer,
+        </span>{" "}
+        dedicated to crafting visually stunning, pixel-perfect, and interactive
+        websites. With a strong foundation in both frontend and backend
+        technologies, I specialize in creating seamless user experiences that
+        bring digital visions to life. My skills extend across the entire stack,
+        allowing me to deliver comprehensive and innovative solutions. Eager to
+        explore new tools and techniques, I am committed to turning ideas into
+        reality and creating exceptional digital experiences.
+      </p>
+      <button className="flex justify-start">
+        <Link
+          to="mailto: faizanejazshaikh%40gmail%2Ecom"
+          className="font-medium tracking-tighter rounded-full bg-pink-700 text-white p-2 text-base md:text-lg duration-300 shadow-lg shadow-pink-500 hover:-translate-y-1"
+        >
+          Contact Me
+        </Link>
+      </button>
+    </div>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,56 +1,91 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import {Skills, Home, ProjectTemplate} from './components/index.js'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Skills, Home, ProjectTemplate } from "./components/index.js";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     children: [
       {
-        path: 'pagepalette',
-        element: <ProjectTemplate 
-                    heading={'Page Palette'} 
-                    description={'A dynamic web application skillfully crafted with React and JavaScript. Uncover an extensive collection of books fetched from the Google Books API. Seamlessly explore genres, utilize the search feature to find your next read, and effortlessly bookmark favorites for later. Delve into detailed views for an informed and delightful reading experience.'} 
-                    site={'https://page-palette.netlify.app/'} 
-                    code={'https://github.com/IronJosh786/PagePalette'}
-                  />
+        path: "youtube-backend",
+        element: (
+          <ProjectTemplate
+            heading={"Youtube Backend"}
+            description={
+              "Built on Node.js and Express.js, this backend project provides a solid foundation for a video content management system. Leveraging MongoDB for data storage and Cloudinary for efficient multimedia asset management, it ensures a scalable and reliable backend infrastructure. Seamlessly handle user authentication, video uploads, likes, comments, and subscriptions. The project focuses on security and performance, utilizing these technologies to create a robust backend for your video platform."
+            }
+            code={"https://github.com/IronJosh786/Backend"}
+            tech={[
+              "MongoDB",
+              "Mongoose",
+              "Express.js",
+              "Node.js",
+              "Bcrypt",
+              "JWT",
+              "Cloudinary",
+              "Multer",
+              "Aggregation Pipeline",
+            ]}
+          />
+        ),
       },
       {
-        path: 'ecommerce',
-        element: <ProjectTemplate 
-                    heading={'Ecommerce Shop'} 
-                    description={'A dynamic Ecommerce Application built using React and JavaScript. It provides a modern and responsive design for optimal user interaction across various devices. The application leverages the Context API for streamlined state management.'} 
-                    site={'https://frontend-clothing-shop.netlify.app/'} 
-                    code={'https://github.com/IronJosh786/Ecommerce-Frontend-App'}
-                  />
+        path: "pagepalette",
+        element: (
+          <ProjectTemplate
+            heading={"Page Palette"}
+            description={
+              "A dynamic web application skillfully crafted with React and JavaScript. Uncover an extensive collection of books fetched from the Google Books API. Seamlessly explore genres, utilize the search feature to find your next read, and effortlessly bookmark favorites for later. Delve into detailed views for an informed and delightful reading experience."
+            }
+            site={"https://page-palette.netlify.app/"}
+            code={"https://github.com/IronJosh786/PagePalette"}
+            tech={[
+              "React.js",
+              "React Router DOM",
+              "Local Storage",
+              "Google Books API",
+              "Tailwind CSS",
+            ]}
+          />
+        ),
       },
       {
-        path: 'todo',
-        element: <ProjectTemplate 
-                    heading={'To Do'} 
-                    description={'A user-friendly React-based application designed for efficient state management through Redux Toolkit. This application empowers users with a seamless interface to effortlessly add, delete, and update todos, providing a streamlined and intuitive experience for managing tasks. Users can enjoy a responsive and organized platform to enhance their productivity and task management efficiency.'} 
-                    site={'https://superb-dieffenbachia-c58623.netlify.app/'} 
-                    code={'https://github.com/IronJosh786/Todo-Redux-Toolkit'}
-                  />
+        path: "ecommerce",
+        element: (
+          <ProjectTemplate
+            heading={"Ecommerce Shop"}
+            description={
+              "A dynamic Ecommerce Application built using React and JavaScript. It provides a modern and responsive design for optimal user interaction across various devices. The application leverages the Context API for streamlined state management."
+            }
+            site={"https://frontend-clothing-shop.netlify.app/"}
+            code={"https://github.com/IronJosh786/Ecommerce-Frontend-App"}
+            tech={[
+              "React.js",
+              "React Router DOM",
+              "Fake Store API",
+              "Tailwind CSS",
+            ]}
+          />
+        ),
       },
       {
-        path: 'skills',
-        element: <Skills />
+        path: "skills",
+        element: <Skills />,
       },
       {
-        path: '',
-        element: <Home />
-      }
-    ]
-  }
-])
+        path: "",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
