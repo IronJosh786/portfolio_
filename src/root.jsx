a3d301de1e63c88f894a4a32fd5cbbfb3fe8d973
Faizan <sfaizan78692@gmail.com>
2024-12-08 21:44:54 +0530

    add open graph image

--- src/root.jsx
+++ src/root.jsx
@@ -8,6 +8,11 @@ export const meta = () => [
     content:
       "Hi, Faizan here! I'm a Full Stack developer crafting visually appealing and highly functional web experiences.",
   },
+  {
+    property: "og:image",
+    content:
+      "https://drive.google.com/file/d/1080OoN4b2oNPiJgiK4XzOip7Fn5Ete6p/view?usp=sharing",
+  },
 ];
 
 export const links = () => [
